589996015e05c99d8632b80dd6af3ab648fcf1cc
jandriw <jan.barcos@gmail.com>
2025-02-16 20:26:19 +0100

    3.4

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import "../app.css"
-  import { page } from "$app/stores"
+  import "../app.css";
+  import { page } from "$app/stores";
   import {
     Navbar,
     NavBrand,
@@ -8,58 +8,66 @@
     NavUl,
     NavLi,
     Button,
-  } from "flowbite-svelte"
-  import type { LayoutData } from "./$types"
-  import { onMount } from "svelte"
-  import { invalidate } from "$app/navigation"
-  
+    Dropdown,
+    Chevron,
+    DropdownItem,
+  } from "flowbite-svelte";
+  import type { LayoutData } from "./$types";
+  import { onMount } from "svelte";
+  import { invalidate } from "$app/navigation";
   const navigation = [
     { label: "Home", href: "/" },
     { label: "Pricing", href: "/pricing" },
     { label: "Contacts", href: "/contacts" },
-    { label: "Account", href: "/account" },
-  ]
-  export let data: LayoutData
-  $: ({ session, supabase } = data)
+  ];
+  export let data: LayoutData;
+  $: ({ session, supabase } = data);
   onMount(() => {
     const {
-        data: { subscription },
+      data: { subscription },
     } = supabase.auth.onAuthStateChange((event, _session) => {
-        if (_session?.expires_at !== session?.expires_at) {
-            invalidate("supabase:auth")
-        }
-    })
-    return () => subscription.unsubscribe()
-  })
+      if (_session?.expires_at !== session?.expires_at) {
+        invalidate("supabase:auth");
+      }
+    });
+    return () => subscription.unsubscribe();
+  });
 </script>
 
 <svelte:head>
-    <title>Contactly</title>
+  <title>Contactly</title>
 </svelte:head>
+
 <div class="flex h-full flex-col">
   <Navbar let:hidden let:toggle>
     <NavBrand href="/">
-      <img
-        src="/images/logo.png"
-        class="mr-3 h-6 sm:h-9"
-        alt="Contactly Logo" />
-      <span
-        class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-        Contactly
-      </span>
+      <img src="/images/logo.png" class="mr-3 h-6 sm:h-9" alt="Contactly Logo" />
+      <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"> Contactly </span>
     </NavBrand>
     <div class="flex md:order-2">
-      <div class="flex items-center gap-2">
-        <Button href="/login" size="sm">Login</Button>
-        <Button href="/register" size="sm" color="alternative"
-            >Register</Button>
-      </div>
+      {#if session}
+        <Button color="light"><Chevron>Account</Chevron></Button>
+        <Dropdown>
+          <div slot="header" class="px-4 py-2">
+            <span class="block truncate text-sm font-medium"> {session.user.email} </span>
+          </div>
+          <DropdownItem href="/account">Settings</DropdownItem>
+          <DropdownItem href="/account">Billing</DropdownItem>
+          <form action="/logout" method="POST">
+            <DropdownItem type="submit" slot="footer">Sign out</DropdownItem>
+          </form>
+        </Dropdown>
+      {:else}
+        <div class="flex items-center gap-2">
+          <Button href="/login" size="sm">Login</Button>
+          <Button href="/register" size="sm" color="alternative">Register</Button>
+        </div>
+      {/if}
       <NavHamburger on:click={toggle} />
     </div>
     <NavUl {hidden}>
       {#each navigation as nav}
-          <NavLi href={nav.href} active={$page.url.pathname === nav.href}
-              >{nav.label}</NavLi>
+        <NavLi href={nav.href} active={$page.url.pathname === nav.href}>{nav.label}</NavLi>
       {/each}
     </NavUl>
   </Navbar>
