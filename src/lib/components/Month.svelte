7a4c2cbab20ee6c1fe1d40a88231d8647929757f
jandriw <jan.barcos@gmail.com>
2025-03-04 00:53:15 +0100

    feat: display month and year in Month component

--- src/lib/components/Month.svelte
+++ src/lib/components/Month.svelte
@@ -8,6 +8,7 @@
   export let completedDays = []; // Recibe las fechas marcadas como 'YYYY-MM-DD'
 
   const daysOfWeek = ["M", "T", "W", "Th", "F", "S", "Su"];
+  const monthNames = ["January", "February", "March", "April", "May", "June", "July", "Agoust", "September", "October", "November", "December"]
 
   let firstDay = new Date(year, month, 1).getDay();
   firstDay = firstDay === 0 ? 6 : firstDay - 1; // Ajuste para que inicie en lunes
@@ -21,7 +22,39 @@
   }
 </script>
 
+<div class="main-container">
+  <p class="month-header">{monthNames[month]} {year}</p>
+  <div class="month-container">
+    {#each daysOfWeek as day}
+      <div class="day-label">{day}</div>
+    {/each}
+
+    {#each Array(firstDay) as _}
+      <div class="empty"></div>
+    {/each}
+
+    {#each Array(daysInMonth) as _, i}
+      <Checkbox 
+        date={new Date(year, month, i + 1)} 
+        today={today} 
+        status={completedDays.includes(formatDate(new Date(year, month, i + 2))) ? "checked" : "unchecked"}
+      />
+    {/each}
+  </div>
+</div>
+
 <style>
+  .main-container {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 2px;
+  }
+
+  .month-header {
+    text-align: center;
+    place-content: center;
+    background-color: rgb(34, 77, 157);
+  }
   .month-container {
     display: grid;
     grid-template-columns: repeat(7, 1fr);
@@ -44,21 +77,3 @@
     height: 28px;
   }
 </style>
-
-<div class="month-container">
-  {#each daysOfWeek as day}
-    <div class="day-label">{day}</div>
-  {/each}
-
-  {#each Array(firstDay) as _}
-    <div class="empty"></div>
-  {/each}
-
-  {#each Array(daysInMonth) as _, i}
-    <Checkbox 
-      date={new Date(year, month, i + 1)} 
-      today={today} 
-      status={completedDays.includes(formatDate(new Date(year, month, i + 2))) ? "checked" : "unchecked"}
-    />
-  {/each}
-</div>
